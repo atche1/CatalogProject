48e900e4061f28d04be9154d9f971b489b992124
kkkristiyana <kknikolova_@abv.bg>
2025-04-15 09:51:17 +0300

    Get movies and books

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CatalogProject.Models;
 
 namespace CatalogProject.Services
 {
-    internal class BookService
+    public class BookService
     {
+        //private readonly CatalogContext catalogContext;
+
+        public void FilterBookByCategory(string categoryName)
+        {
+
+        }
+        public List<Book> GetAllBooks()
+        {
+            using (var catalogContext = new CatalogContext())
+            {
+                List<Book> books = catalogContext.Books.ToList();
+                return books;
+            }
+        }
     }
 }
---
+++ Services/MovieService.cs
@@ -0,0 +1,21 @@
+﻿using CatalogProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogProject.Services
+{
+    public class MovieService
+    {
+        public List<Movie> GetAllMovies()
+        {
+            using (var catalogContext = new CatalogContext())
+            {
+                List<Movie> movies = catalogContext.Movies.ToList();
+                return movies;
+            }
+        }
+    }
+}
