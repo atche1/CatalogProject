d55b21787efb7ea4b15aa962c172ddc6a3a60093
atche1 <atche1202@gmail.com>
2025-04-22 09:55:17 +0300

    aaa

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CatalogProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogProject.Services
 {
@@ -11,9 +12,30 @@ namespace CatalogProject.Services
     {
         //private readonly CatalogContext catalogContext;
 
-        public void FilterBookByCategory(string categoryName)
+        public void FilterBookByGenre(string genreName)
         {
-
+            using (var catalogContext = new CatalogContext())
+            {
+                var category = catalogContext.Movies.Include(m => m.Genre).FirstOrDefault(m => m.Genre.GenreName == genreName);
+            }
+        }
+        static void PrintFilmsByCategoryName()
+        {
+            string categoryName = Console.ReadLine();
+            var category = context.Categories.Include(c => c.FilmCategories).ThenInclude(c => c.Film).FirstOrDefault(c => c.Name == categoryName);
+            if (category != null)
+            {
+                foreach (var fc in category.FilmCategories)
+                {
+                    Console.WriteLine(fc.Film.Title);
+                }
+            }
+            else
+            {
+                Category newCategory = new Category() { Name = categoryName };
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+            }
         }
         public List<Book> GetAllBooks()
         {
