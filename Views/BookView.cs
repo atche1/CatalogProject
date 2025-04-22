1ccabc037c565b0a0a0c1723be533fad8db26c72
atche1 <atche1202@gmail.com>
2025-04-22 08:36:30 +0300

    add book view

---
+++ Views/BookView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogProject.Services;
+using CatalogProject.Models;
+
+namespace CatalogProject.Views
+{
+    public class BookView
+    {
+        private static BookService bookService = new BookService();
+        public void ShowAllBooks()
+        {
+            List<Book> books = bookService.GetAllBooks();
+            foreach (var book in books)
+            {
+                
+            }
+        }
+    }
+}
